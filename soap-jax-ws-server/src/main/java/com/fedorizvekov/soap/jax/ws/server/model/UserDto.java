e3c7b087df9946c7954d3e41d946ecc400b82839
FedorIzvekov <fedor.izvekov@gmail.com>
2016-10-02 15:02:13 +0300

    soap-jax-ws-serve: added lombok

--- soap-jax-ws-server/src/main/java/com/fedorizvekov/soap/jax/ws/server/model/UserDto.java
+++ soap-jax-ws-server/src/main/java/com/fedorizvekov/soap/jax/ws/server/model/UserDto.java
@@ -1,5 +1,12 @@
 package com.fedorizvekov.soap.jax.ws.server.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(exclude = {"password"})
 public class UserDto {
 
     private String email;
@@ -7,71 +14,5 @@ public class UserDto {
     private String phoneNumber;
     private String firstName;
     private String lastName;
-
-
-    public String getEmail() {
-        return email;
-    }
-
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    public String getPassword() {
-        return password;
-    }
-
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-
-    public String getLastName() {
-        return lastName;
-    }
-
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-
-    @Override
-    public String toString() {
-        return new StringBuilder().append("email: ")
-                .append(email)
-                .append(", phoneNumber: ")
-                .append(phoneNumber)
-                .append(", password: ")
-                .append(password)
-                .append(", firstName: ")
-                .append(firstName)
-                .append(", lastName: ")
-                .append(lastName)
-                .toString();
-    }
 
 }
--- soap-jax-ws-server/src/main/java/com/fedorizvekov/soap/jax/ws/server/service/RegistrationServiceImpl.java
+++ soap-jax-ws-server/src/main/java/com/fedorizvekov/soap/jax/ws/server/service/RegistrationServiceImpl.java
@@ -2,19 +2,16 @@ package com.fedorizvekov.soap.jax.ws.server.service;
 
 import javax.jws.WebService;
 import com.fedorizvekov.soap.jax.ws.server.model.UserDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @WebService(endpointInterface = "com.fedorizvekov.soap.jax.ws.server.service.RegistrationService")
 public class RegistrationServiceImpl implements RegistrationService {
-
-    Logger log = LoggerFactory.getLogger(RegistrationServiceImpl.class);
 
     @Override
     public String registration(UserDto userDto) {
 
         log.info("REQUEST POST, endpoint /registration was called");
-
-        return "REGISTRATION COMPLETED SUCCESSFULLY, user: " + userDto;
+        return "REGISTRATION COMPLETED, user: " + userDto;
     }
 }
